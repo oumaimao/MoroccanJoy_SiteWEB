9e6cf96b86485791fb22bbf7c2e1a8ee0ecc0582
soufiane <soufianeounjli@gmail.com>
2024-03-28 21:56:18 +0000

    u

--- website_code/event.sql
+++ website_code/event.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2024 at 02:53 AM
+-- Generation Time: Mar 28, 2024 at 09:44 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,138 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `A_email` varchar(100) NOT NULL,
+  `A_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `Categorie_id` int(11) NOT NULL,
+  `Nom_cat` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `demande_evenement`
+--
+
+CREATE TABLE `demande_evenement` (
+  `Demande_id` int(11) NOT NULL,
+  `E_Name` varchar(30) NOT NULL,
+  `Event_Date` date NOT NULL,
+  `E_Time` time NOT NULL,
+  `E_Duration` varchar(5) NOT NULL,
+  `User_id` int(11) NOT NULL,
+  `E_image` varchar(20) NOT NULL,
+  `E_description` varchar(400) NOT NULL,
+  `E_localisation` varchar(100) NOT NULL,
+  `E_Adresse1` varchar(250) NOT NULL,
+  `E_Adresse2` varchar(250) NOT NULL,
+  `E_country` varchar(20) NOT NULL,
+  `E_city` varchar(20) NOT NULL,
+  `E_zipcode` varchar(10) NOT NULL,
+  `Categorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `E_id` int(11) NOT NULL,
+  `Titre` varchar(100) NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Image` varchar(500) NOT NULL,
+  `Heure_debut` time NOT NULL,
+  `Date_debut` date NOT NULL,
+  `Heure_fin` time NOT NULL,
+  `Date_fin` date NOT NULL,
+  `localisation` varchar(100) NOT NULL,
+  `Prix_ticket` float NOT NULL,
+  `Nombre_tickets` int(11) NOT NULL,
+  `Ville` varchar(30) NOT NULL,
+  `Zip_code` varchar(30) NOT NULL,
+  `Categorie_id` int(11) DEFAULT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+
+CREATE TABLE `image` (
+  `image_id` int(11) NOT NULL,
+  `img_Pf` varchar(300) NOT NULL,
+  `img_Cv` varchar(500) NOT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reclamation`
+--
+
+CREATE TABLE `reclamation` (
+  `Reclammation_id` int(11) NOT NULL,
+  `Contact_Name` varchar(30) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Subject` varchar(100) NOT NULL,
+  `Description` varchar(250) NOT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `Reservation_id` int(11) NOT NULL,
+  `R_Fname` varchar(30) NOT NULL,
+  `R_Lname` varchar(30) NOT NULL,
+  `R_email` varchar(100) NOT NULL,
+  `R_adresse` varchar(250) NOT NULL,
+  `R_country` varchar(30) NOT NULL,
+  `R_city` varchar(30) NOT NULL,
+  `R_Zipcode` varchar(20) NOT NULL,
+  `Ticket_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `Ticket_id` int(11) NOT NULL,
+  `Nombre_ticket` int(11) NOT NULL,
+  `QR_code` varchar(100) NOT NULL,
+  `Statu` tinyint(1) NOT NULL,
+  `User_id` int(11) NOT NULL,
+  `E_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -39,8 +171,72 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`User_id`, `U_name`, `U_Prenom`, `U_email`, `U_password`, `U_telephone`, `U_adresse`, `U_dateNaissance`) VALUES
+(1, 'sliman', 'isslam', 'slimane@gmail.com', '$2y$10$CqBOM5wWQiB/4', '', '', '0000-00-00');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`Categorie_id`);
+
+--
+-- Indexes for table `demande_evenement`
+--
+ALTER TABLE `demande_evenement`
+  ADD PRIMARY KEY (`Demande_id`),
+  ADD KEY `Categorie_id` (`Categorie_id`),
+  ADD KEY `User_id` (`User_id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`E_id`),
+  ADD KEY `Categorie_id` (`Categorie_id`),
+  ADD KEY `event_ibfk_2` (`User_id`);
+
+--
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `User_id` (`User_id`);
+
+--
+-- Indexes for table `reclamation`
+--
+ALTER TABLE `reclamation`
+  ADD PRIMARY KEY (`Reclammation_id`),
+  ADD KEY `User_id` (`User_id`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`Reservation_id`),
+  ADD KEY `Ticket_Of_id` (`Ticket_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`Ticket_id`),
+  ADD KEY `User_id` (`User_id`),
+  ADD KEY `E_OF_id` (`E_id`);
 
 --
 -- Indexes for table `user`
@@ -53,10 +249,101 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `Categorie_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `demande_evenement`
+--
+ALTER TABLE `demande_evenement`
+  MODIFY `Demande_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `E_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reclamation`
+--
+ALTER TABLE `reclamation`
+  MODIFY `Reclammation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `Reservation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `Ticket_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `User_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `User_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `demande_evenement`
+--
+ALTER TABLE `demande_evenement`
+  ADD CONSTRAINT `demande_evenement_ibfk_1` FOREIGN KEY (`Categorie_id`) REFERENCES `categorie` (`Categorie_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `demande_evenement_ibfk_2` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`Categorie_id`) REFERENCES `categorie` (`Categorie_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `event_ibfk_2` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `image`
+--
+ALTER TABLE `image`
+  ADD CONSTRAINT `image_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reclamation`
+--
+ALTER TABLE `reclamation`
+  ADD CONSTRAINT `reclamation_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`Ticket_id`) REFERENCES `ticket` (`Ticket_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `user` (`User_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`E_id`) REFERENCES `event` (`E_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
